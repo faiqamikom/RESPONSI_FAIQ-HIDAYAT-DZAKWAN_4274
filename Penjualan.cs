d72abaf6ed8b49f1f78c97da8bacbf825cd0bfed
faiqamikom <101610607+faiqamikom@users.noreply.github.com>
2022-07-12 16:55:48 +0700

    Update Penjualan.cs

--- Penjualan.cs
+++ Penjualan.cs
@@ -8,13 +8,14 @@ namespace ProjectPenjualan
 {
 	public class Penjualan
 	{
-		public string Nota { get; set; }
+		//MENGAPA DI VS SAYA JIKA TIDAK ADA "?" ADA PERINGATANNYA YA PAK?
+		public string? Nota { get; set; }
 
-		public string Tanggal { get; set; }
+		public string? Tanggal { get; set; }
 
-		public string Customer { get; set; }
+		public string? Customer { get; set; }
 
-		public string Jenis { get; set; }
+		public string? Jenis { get; set; }
 
 		public int Total_Nota { get; set; }
 	}
